519d8f7075d5b5efae6bdcb11d9d4ae1e0ed57e6
Oladotun Denton <dotundenton@gmail.com>
2021-06-08 17:34:08 +0100

    Added a Makefile ,migrations

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
